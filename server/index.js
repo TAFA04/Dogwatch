7ab8f31e4228022d7d3aba5ae638a2b8268cdaa6
d-berg <d.berg@3kstudios.com>
2018-05-17 23:28:38 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,52 +3,91 @@ const sequelize = require('sequelize')
 const User = require('./users/model')
 const Breed = require('./breedlikes/model')
 const app = express()
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 /*const Users = {
+
   1: {
+
     id: 1,
+
     name: "Henk",
+
     email: "breadlover@gosia.com",
+
     password: "123456"
+
   },
+
   2: {
+
     id: 2,
+
     name: "Tim",
+
     email: "timtim@bread.com",
+
     password:"blabla"
+
   }
+
 }
+
+
 
 const Breeds = {
+
   1: {
+
     id: 1,
+
     breed: "schnauzer",
+
   },
+
   2: {
+
     id: 2,
+
     breed: "beagle"
+
   }
+
 }
 
+
+
 const BreedLikes = {
+
   1: {
+
     id: 1,
+
     userid: 1,
+
     breedid: 1,
+
     likes: 5
+
   },
+
   2: {
+
     id: 2,
+
     userid: 3,
+
     breedid: 2,
+
     likes: 6
+
   }
 
-}*/
 
+
+}*/
 
 app.listen(4001, () => console.log('Express API listening on port 4001'))
 
@@ -64,8 +103,12 @@ app.get('/do-something', (request, response) => {
   response.end()
 });
 
-app.get('/users', (req, res) => {
-  User.findAll()
+app.get('/API/users/:id', (req, res) => {
+  const id = req.params.id;
+  User.findOne({
+    attributes: ['username','email'],
+    where: {id: id }
+  })
   .then(result => {
     res.send({
       users: result
@@ -84,7 +127,6 @@ app.get('/API/pref/:userid', (req, res) => {
   })
 Breed.findOne({
   attributes: ['breedname'],
-
 where: {likes: likemax} && {userid: userid}
 })
   .then(result => {
@@ -96,28 +138,49 @@ where: {likes: likemax} && {userid: userid}
   })
 })
 
-app.post('/API/top', (req, res) => {
-  const banana = req.body.banana;
-  const currentuserid = req.body.currentuserid;
-  const notcurrentuser = {[sequelize.Op.notIn]: [currentuserid]}
+app.get('/API/top', (req, res) => {
+  const banana = "schnauzer"; //req.body.banana;
+  //const currentuserid = 1;//req.body.currentuserid;
+  //const notcurrentuser = {[sequelize.Op.notIn]: [currentuserid]}
   /*const likemax = Breed.max('likes', {
     where: {breedname: banana}
   })
   console.log(likemax)*/
 Breed.findAll({
-  //attributes: ['userid'],
-  /*where: {
-    userid:notcurrentuser,
-    breedname:banana,
-    likes:
-  }*/
-where: {userid: notcurrentuser} && {breedname: banana} 
+  attributes: ['userid'],
+  where: {breedname: banana}
 })
   .then(result => {
-   res.send({result})
+   res.send(result)
   })
   .catch(err => {
     // there was an error, return some HTTP error code
     res.status(500).send({error: 'Something went wrong with Postgres'})
   })
+})
+
+app.post('/API/like', (req, res) =>{
+  Breed.count({where: {userid:req.body.userid, breedname: req.body.breed}}).then((count) => {
+    if(count !== 0){
+      console.log('increment!');
+        Breed.increment('likes',{
+          where: {
+            userid: req.body.userid,
+            breedname: req.body.breed 
+          }
+        })
+    } else {
+      console.log('create!');
+      Breed.create({
+        userid: req.body.userid,
+        breedname: req.body.breed,
+        likes: 1
+      })
+    }
+  })
+
+  .then(result => {
+    return res
+})
+res.end()
 })
