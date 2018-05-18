89a933bf7d6de63d6a7945b72c96488b14a63834
d-berg <d.berg@3kstudios.com>
2018-05-18 12:52:53 +0200

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,7 +2,7 @@ import image from './image'
 import pref from './pref'
 import matches from './matches'
 import user from './user'
-import counter from '.counter'
+import counter from './counter'
 
 export default {
   image, pref, matches, user, counter
